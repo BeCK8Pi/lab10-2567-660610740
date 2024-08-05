65e6aac5284e708b03b0f3d5a50b59cc57c1c079
BeCK8Pi <khuntip1207@gmail.com>
2024-08-05 20:26:08 +0700

    finishmunk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
 "use client";
 
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { cleanUser } from "@/libs/cleanUser";
+import UserCard from "@/components/UserCard";
+import { UserCardProps } from "@/libs/types";
 
 export default function RandomUserPage() {
   // annotate type for users state variable
-  const [users, setUsers] = useState(null);
+  const [users, setUsers] = useState<UserCardProps[]>([]);
 
   const [isLoading, setIsLoading] = useState(false);
-  const [genAmount, setGenAmount] = useState(1);
+  const [genAmount, setGenAmount] = useState<number>();
 
   const generateBtnOnClick = async () => {
     setIsLoading(true);
@@ -20,8 +23,29 @@ export default function RandomUserPage() {
 
     //Your code here
     //Process result from api response with map function. Tips use function from /src/libs/cleanUser
+    const got = users.map((x:any)=>cleanUser(x));
     //Then update state with function : setUsers(...)
+    setUsers(got);
+    console.log(got);
   };
+
+  const [isFirstLoad,setIsFirstLoad] = useState(true);
+
+  useEffect(()=>{
+    if(isFirstLoad){
+      setIsFirstLoad(false);
+      return;
+    }
+    localStorage.setItem("getAmount",JSON.stringify(genAmount));
+  },[genAmount])
+
+  useEffect(()=>{
+    const amount = localStorage.getItem("getAmount");
+    if(amount){
+      const gen = JSON.parse(amount);
+      setGenAmount(gen);
+    }
+  },[])
 
   return (
     <div style={{ maxWidth: "700px" }} className="mx-auto">
@@ -42,7 +66,7 @@ export default function RandomUserPage() {
       {isLoading && (
         <p className="display-6 text-center fst-italic my-4">Loading ...</p>
       )}
-      {users && !isLoading && users.map(/*code map rendering UserCard here */)}
+      {users && !isLoading && users.map((x)=><UserCard name={x.name} imgUrl={x.imgUrl} address={x.address} email={x.email}/>)}
     </div>
   );
 }
